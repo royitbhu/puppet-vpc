84b474f69a50d8e70a57d76fb427a5990de07416
Saju Madhavan <sajuptpm@gmail.com>
2017-02-18 10:33:14 +0530

    staging repo change

--- manifests/omd_client.pp
+++ manifests/omd_client.pp
@@ -1,6 +1,6 @@
 class rjil::omd_client (
   $check_mk_version = '1.2.6p12-1',
-  $download_source  = 'http://10.140.214.61/apt/sajuptpm/vpcmainprod/pool/main/c/check-mk-agent/',
+  $download_source  = 'http://10.140.214.61/apt/vpcjiocloud/vpcbase/pool/main/c/check-mk-agent/',
 ) {
   class { '::omd::client':
     check_mk_version => $check_mk_version,
