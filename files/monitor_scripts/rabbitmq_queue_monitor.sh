95559531a4b0b5b75b82d9d0902428d1ce47a739
Rahul Jain <rahuljain.roy@gmail.com>
2017-03-28 12:11:21 +0000

    Adding script for monitoring rabbitmq queueSize (JNT-389)

---
+++ files/monitor_scripts/rabbitmq_queue_monitor.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+import re
+import time
+
+INTERVAL = 3
+ITERATION = 5
+hostName = []
+queueSize = [[] for _ in range(ITERATION)]
+totalQueues = 0
+
+STATUS = {"success" : 0,
+    "warning" : 1,
+    "critical" : 2,
+     }
+
+OUTPUT_FORMAT = "{status} {entity} - {message}"
+
+def getQueueSize(x):
+        cmd = "rabbitmqctl list_queues"
+        out = os.popen(cmd).read()
+        global totalQueues
+        #totalQueues = out.count('\n')
+        lines = out.splitlines()
+        pattern = '(\S+)\s+(\d+)'
+	i = 0
+        for line in lines:
+                matchObj = re.match(pattern, line, re.M|re.I)
+                if matchObj:
+                        if x == 0:
+                                hostName.append(matchObj.group(1))
+                                totalQueues = totalQueues + 1
+                        queueSize[x].append(int(matchObj.group(2)))
+        return
+
+for i in range(ITERATION):
+        getQueueSize(i)
+        time.sleep(INTERVAL);
+
+#Verify if any queue is continuously growing
+for i in range(totalQueues):
+        flag = 0
+	msg = ""
+        for j in range(ITERATION-1):
+                if queueSize[j][i] == 0:
+                        flag = 0
+                        break;
+                elif queueSize[j][i] <= queueSize[j+1][i]:
+                        flag = 1
+                else:
+                        flag = 0
+                        break;
+        if flag == 1:
+		msg = "CRITICAL: " + "Queue for host {} not getting consumed, Please take appropiate action.".format(hostName[i])
+                status = STATUS.get("critical")
+        else:
+		msg = "OK: " + "Queue for host {} is normal, No action required.".format(hostName[i])
+                status = STATUS.get("success")
+	print OUTPUT_FORMAT.format(status=status, entity="RabbitMQ:QueueSize", message=msg)
